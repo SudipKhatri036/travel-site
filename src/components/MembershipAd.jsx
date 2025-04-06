782e2fdb49ca99734b5bacc6053a4de318ee0d16
Sudip Khatri <sudipkhatri036@gmail.com>
2025-04-06 20:47:03 +0545

    Added membership ad section

---
+++ src/components/MembershipAd.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function MembershipAd() {
+  return (
+    <section className="max-w-7xl mx-auto relative px-6 py-8 mb-16 rounded-2xl">
+      <div className="w-1/2 px-[42px] py-6 flex flex-col justify-center relative z-[12]">
+        <h2 className="capitalize mb-2 text-2xl text-white font-bold">
+          Become member
+        </h2>
+        <p className="text-white text-base">
+          Register now to unlock exclusive features and enjoy amazing promos!
+        </p>
+        <ul className="grid grid-cols-2 mt-4 mb-8 gap-1">
+          <li className="flex items-center gap-2 text-white text-sm">
+            <span>
+              <img src="./images/icons/round-tick.png" alt="round right" />
+            </span>
+            Special app discount
+          </li>
+          <li className="flex items-center gap-2 text-white text-sm">
+            <span>
+              <img src="./images/icons/round-tick.png" alt="round right" />
+            </span>
+            Travel Benefits
+          </li>
+          <li className="flex items-center gap-2 text-white text-sm">
+            <span>
+              <img src="./images/icons/round-tick.png" alt="round right" />
+            </span>
+            Best deals
+          </li>
+          <li className="flex items-center gap-2 text-white text-sm">
+            <span>
+              <img src="./images/icons/round-tick.png" alt="round right" />
+            </span>
+            Access to all features
+          </li>
+        </ul>
+
+        <button className="w-1/2 px-4 py-2.5 rounded-lg border-[1px] border-border-secondary bg-bg-secondary  cursor-pointer">
+          Become Member
+        </button>
+      </div>
+      <div className="absolute w-full h-full top-0 z-[11]  bg-gradient-to-r from-[#033FB8] from-0% to-[#276ffe00] to-100% rounded-2xl"></div>
+      <div className="bg-[url(./images/membership/membership-ad-hero.png)] absolute w-full h-full top-0 z-10  px-6 object-cover bg-no-repeat"></div>
+    </section>
+  );
+}
+
+export default MembershipAd;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Promotion from "../components/Promotion";
 import PopularDestination from "../components/PopularDestination";
+import MembershipAd from "../components/MembershipAd";
 
 function Home() {
   return (
     <div>
       <Promotion />
       <PopularDestination />
+      <MembershipAd />
     </div>
   );
 }
